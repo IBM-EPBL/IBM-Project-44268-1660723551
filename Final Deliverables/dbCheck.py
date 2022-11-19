d84fb260522b0ad9da1380f889d5a96e4b567cc2
harish-2306 <113781479+harish-2306@users.noreply.github.com>
2022-11-19 22:44:51 +0530

    Add files via upload

---
+++ Final Deliverables/dbCheck.py
@@ -0,0 +1,14 @@
+import ibm_db
+
+connection = ibm_db.connect("DATABASE=bludb;HOSTNAME=98538591-7217-4024-b027-8baa776ffad1.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=30875;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=lgm69103;PWD=3C3EUtNxNUE5zoCL",'','')
+
+print(connection)
+print("Connection Successfull !\n\n")
+
+sql = "SELECT EMAIL,PASSWORD FROM logins"
+stmt = ibm_db.exec_immediate(connection, sql)
+dictionary = ibm_db.fetch_assoc(stmt)
+while dictionary != False:
+    # printing
+    print("Full Row : ", dictionary)
+    dictionary = ibm_db.fetch_assoc(stmt)
